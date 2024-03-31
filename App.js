d42f20878645554484759fbcd326198cbeba65d6
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-03-31 21:08:46 +0530

    Implement native stack navigator
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
-import { StyleSheet, View, StatusBar } from 'react-native';
-import Home from './src/screens/Home';
+import { StyleSheet, View, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/screens/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar backgroundColor={'#fff'}  barStyle="dark-content" />
-      <Home/>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <StatusBar backgroundColor={"#fff"} barStyle="dark-content" />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
