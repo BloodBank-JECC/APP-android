90e6948349a9e60ff6d7e1b1f2fb80c5c859ed5c
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-04-12 21:44:32 +0530

    Implement a module to access image from gallery
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

---
+++ src/utils/ImagePickerGallery.js
@@ -0,0 +1,46 @@
+import { Alert } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import ShowToast from "../components/Toast";
+
+const getFileSize = async (uri) => {
+  const fileInfo = await FileSystem.getInfoAsync(uri);
+  const fileSize = fileInfo.size / (1024 * 1024);
+  return fileSize;
+};
+
+const ImagePickerGallery = async () => {
+  const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+  if (status !== "granted") {
+    Alert.alert(
+      "Permission Denied",
+      "Sorry, we need camera roll permissions to make this work!",
+      [{ text: "OK" }]
+    );
+    return;
+  }
+
+  const result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    aspect: [1, 1],
+    quality: 0.5,
+  });
+
+  console.log("result: " + result.assets[0].uri);
+
+  if (!result.canceled) {
+    const fileSize = await getFileSize(result.assets[0].uri);
+    if (fileSize > 2) {
+      ShowToast(
+        "error",
+        "File size exceeds 2MB. Please choose a smaller image."
+      );
+    } else {
+        return result.assets[0].uri;
+    }
+  }
+};
+
+export default ImagePickerGallery;
