fc2bbc09ef40cbb9d1df214d464482bfd77740fb
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-04-13 11:40:07 +0530

    Check user is already logged in and redirect to screens accordingly
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/screens/Startup.js
+++ src/screens/Startup.js
@@ -1,9 +1,46 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  StyleSheet,
+} from "react-native";
 import LottieView from "lottie-react-native";
 import { useNavigation } from "@react-navigation/core";
+import { useState, useEffect } from "react";
+import database from "@react-native-firebase/database";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useUser } from "../services/UserContext";
 
 export default function Startup() {
   const navigation = useNavigation();
+  const { setUser } = useUser();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    checkLoggedInStatus();
+  }, []);
+
+  const checkLoggedInStatus = async () => {
+    try {
+      const userId = await AsyncStorage.getItem("userId");
+      if (userId) {
+        const userDetailsSnapshot = await database()
+          .ref(`users/${userId}`)
+          .once("value");
+        const userDetails = userDetailsSnapshot.val();
+
+        if (userDetails) {
+          setUser(userDetails);
+          navigation.replace("Home");
+        }
+      }
+    } catch (error) {
+      console.error("Error checking login status:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <View style={styles.conatiner}>
@@ -14,18 +51,26 @@ export default function Startup() {
         style={styles.lottie}
       />
 
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Login")}
-        style={{
-          width: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <View style={styles.buttonConatiner}>
-          <Text style={styles.text}>Let's Go</Text>
-        </View>
-      </TouchableOpacity>
+      {loading ? (
+        <ActivityIndicator
+          size={"large"}
+          color={"#e75f62"}
+          style={styles.loading}
+        />
+      ) : (
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Login")}
+          style={{
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <View style={styles.buttonConatiner}>
+            <Text style={styles.text}>Let's Go</Text>
+          </View>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
@@ -54,5 +99,8 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: "white",
     fontWeight: "500",
+  },
+  loading: {
+    marginTop: 50,
   },
 });
