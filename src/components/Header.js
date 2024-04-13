3c804f0b6d125ce226baa79a074332a25efd9a9e
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-04-13 11:42:45 +0530

    Link user data to Header component
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import { useUser } from "../services/UserContext";
 
 export default function Header() {
+  const { user } = useUser();
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.welcomeText}>Hello, Amal</Text>
+        <Text style={styles.welcomeText}>Hello, {user.name}</Text>
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <View style={styles.greenLight} />
-          <Text style={styles.locationText}>Thrissur</Text>
+          <Text style={styles.locationText}>{user.location}</Text>
         </View>
       </View>
       <FontAwesome name="bell" size={25} style={styles.icon} />
+      <Image source={{ uri: user.profileImage }} style={styles.profileImage} />
     </View>
   );
 }
@@ -40,4 +44,10 @@ const styles = StyleSheet.create({
   icon: {
     marginLeft: "auto",
   },
+  profileImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    marginLeft: 18,
+  }
 });
