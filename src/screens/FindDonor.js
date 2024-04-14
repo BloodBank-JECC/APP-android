9f5c3ef3c1f368bf77bb33061de51e86f4c2146e
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-04-14 06:06:02 +0530

    Link FindDonor screen to DonorList to show available donors based on selection
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/screens/FindDonor.js
+++ src/screens/FindDonor.js
@@ -1,57 +1,30 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import BloodTypeSlector from "../components/BloodTypeSelector";
 import FilterOptions from "../components/FilterOptions";
+import DonorList from "./DonorList";
+import { useApp } from "../services/AppContext";
 
 export default function FindDonor() {
+  const navigation = useNavigation();
+  const { bloodType } = useApp();
+
   return (
     <View style={styles.container}>
       <BloodTypeSlector />
       <FilterOptions />
 
-      <Text style={styles.title}>Available Donor</Text>
-      <View style={styles.profileConatiner}>
-        <Image source={require("../../assets/anya.png")} style={styles.profileImage} />
-        <View style={{ marginLeft: 15 }}>
-          <Text style={styles.profileNameText}>Anya</Text>
-          <Text style={styles.profileLocationText}>From East</Text>
-        </View>
-        <View style={styles.bookNowConatiner}>
-            <Text style={styles.bookNowtext}>Book Now</Text>
-        </View>
+      <View style={styles.donorListHeader}>
+        <Text style={styles.title}>Available Donor</Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("DonorList")}
+          style={{ marginLeft: "auto" }}
+        >
+          <Text style={styles.viewAllText}>View All</Text>
+        </TouchableOpacity>
       </View>
 
-      <View style={styles.profileConatiner}>
-        <Image source={require("../../assets/anya.png")} style={styles.profileImage} />
-        <View style={{ marginLeft: 15 }}>
-          <Text style={styles.profileNameText}>Anya</Text>
-          <Text style={styles.profileLocationText}>From East</Text>
-        </View>
-        <View style={styles.bookNowConatiner}>
-            <Text style={styles.bookNowtext}>Book Now</Text>
-        </View>
-      </View>
-
-      <View style={styles.profileConatiner}>
-        <Image source={require("../../assets/anya.png")} style={styles.profileImage} />
-        <View style={{ marginLeft: 15 }}>
-          <Text style={styles.profileNameText}>Anya</Text>
-          <Text style={styles.profileLocationText}>From East</Text>
-        </View>
-        <View style={styles.bookNowConatiner}>
-            <Text style={styles.bookNowtext}>Book Now</Text>
-        </View>
-      </View>
-
-      <View style={styles.profileConatiner}>
-        <Image source={require("../../assets/anya.png")} style={styles.profileImage} />
-        <View style={{ marginLeft: 15 }}>
-          <Text style={styles.profileNameText}>Anya</Text>
-          <Text style={styles.profileLocationText}>From East</Text>
-        </View>
-        <View style={styles.bookNowConatiner}>
-            <Text style={styles.bookNowtext}>Book Now</Text>
-        </View>
-      </View>
+      {bloodType && <DonorList />}
     </View>
   );
 }
@@ -62,43 +35,15 @@ const styles = StyleSheet.create({
     padding: 15,
   },
   title: {
-    marginTop: 15,
     fontSize: 18,
   },
-  profileConatiner: {
-    flexDirection: 'row',
-    width: '100%',
-    height: 70,
-    alignItems: 'center',
-    marginVertical: 5,
-    padding: 10,
-    backgroundColor: '#fcfcfc',
-    borderRadius: 10,
-    elevation: 1,
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.29,
-    shadowRadius: 4.65,
+  donorListHeader: {
+    flexDirection: "row",
+    marginTop: 15,
+    alignItems: "baseline",
   },
-  profileImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 10,
+  viewAllText: {
+    marginLeft: "auto",
+    fontSize: 15,
   },
-  profileNameText: {
-    fontSize: 20,
-    fontWeight: '400',
-  },
-  profileLocationText: {
-    fontSize: 14,
-    color: 'grey',
-  },
-  bookNowConatiner: {
-    marginLeft: 'auto',
-    padding: 10,
-    borderWidth: 1,
-    borderRadius: 5,
-  }
 });
