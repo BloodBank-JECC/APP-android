1135f4fb2e6be099a10374947b04d48a12d55885
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-04-29 16:34:24 +0530

    Integrated a better image compressor for profile picture uploads
    * Using expo-image-manipulator.
    
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/utils/ImagePickerGallery.js
+++ src/utils/ImagePickerGallery.js
@@ -1,6 +1,7 @@
 import { Alert } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
+import * as ImageManipulator from "expo-image-manipulator";
 import ShowToast from "../components/Toast";
 
 const getFileSize = async (uri) => {
@@ -25,18 +26,23 @@ const ImagePickerGallery = async () => {
     mediaTypes: ImagePicker.MediaTypeOptions.Images,
     allowsEditing: true,
     aspect: [1, 1],
-    quality: 0.2,
+    quality: 1,
   });
 
   if (!result.canceled) {
-    const fileSize = await getFileSize(result.assets[0].uri);
-    if (fileSize > 2) {
+    const compressedUri = await ImageManipulator.manipulateAsync(
+      result.assets[0].uri,
+      [{ resize: { width: 300, height: 300 } }],
+      { compress: 0.8, format: ImageManipulator.SaveFormat.JPEG }
+    );
+    const fileSize = await getFileSize(compressedUri.uri);
+    if (fileSize > 1) {
       ShowToast(
         "error",
-        "File size exceeds 2MB. Please choose a smaller image."
+        "File size exceeds 1MB. Please choose a smaller image."
       );
     } else {
-        return result.assets[0].uri;
+        return compressedUri.uri;
     }
   }
 };
